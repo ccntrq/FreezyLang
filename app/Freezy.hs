102ed4f3c303756de6385c7c9af46c9fc935ce59
Alex Pankoff <ccntrq@screenri.de>
2017-09-11 18:47:21 +0200

    add a repl

--- app/Freezy.hs
+++ app/Freezy.hs
@@ -3,8 +3,27 @@ module Freezy where
 
 import FreezyLexer
 
+import System.Console.Readline
+import System.Exit
+
 -- | The entry point to the interpreter
 main :: IO ()
 main = do
     print $ "Freezy v0.0.0"
-    print $ runLexer initState $ lexIt "hallo fun plus(a,b){ a+b;}#aada"
+    putStrLn "Welcome to the Freezy repl."
+    putStrLn "Enter 'quit' or hit Ctrl+D to leave"
+    repl -- enter the repl
+
+-- | a repl for Freezy using readline
+repl :: IO ()
+repl = do
+    putStr prompt
+    input <- readline prompt -- read
+    case input of
+        Nothing     -> exitSuccess
+        Just "quit" -> exitSuccess
+        Just input' -> do 
+            addHistory input'
+            print $ runLexer initState $ lexIt input' -- print / eval
+            repl -- loop
+  where prompt = "freezy─➤➤➤"
