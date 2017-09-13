7dfb3962312bff7072bf7c0b56c413c7ccb3e971
Alex Pankoff <ccntrq@screenri.de>
2017-09-13 21:33:28 +0200

    Freezy: add possibility to run a file and arg dispatcher

--- app/Freezy.hs
+++ app/Freezy.hs
@@ -6,12 +6,43 @@ import FreezyParser
 import FreezyEvaluator
 
 import System.Console.Readline
+import System.Environment
 import System.Exit
 
--- | The entry point to the interpreter
+{-* MAIN -}
+
+-- | The main
 main :: IO ()
 main = do
-    print $ "Freezy v0.0.3"
+    args <- getArgs
+    dispatch args
+
+-- | the arg dispatcher
+dispatch :: [String] -> IO ()
+dispatch ["-v"] = version
+dispatch ["-h"] = usage
+dispatch []     = replEntry
+dispatch files =
+   if length files == 1
+       then execFile $ head files
+       else usage
+
+-- | print the version
+version :: IO ()
+version = putStrLn "Freezy v0.0.3"
+
+-- | print version and usage
+usage :: IO ()
+usage = do
+    version
+    putStrLn "Usage: freezy [filename] [-v] [-h]\n"
+
+{- * REPL -}
+
+-- | prints a welcome message and enters the repl
+replEntry :: IO ()
+replEntry = do
+    version
     putStrLn "Welcome to the Freezy repl."
     putStrLn "Enter 'quit' or hit Ctrl+D to leave"
     repl -- enter the repl
@@ -29,16 +60,27 @@ repl = do
             repl
   where prompt = "freezy─➤➤➤"
 
--- | interpret a given source string
+{- * execFile -}
+
+execFile :: String -> IO ()
+execFile name = do
+    source <- readFile name
+    runSource source
+    exitSuccess
+
+{- * Backend -}
+
+-- | interpret a given source string. This is the backend for both the repl
+--   and for execFile
 runSource :: String -> IO ()
 runSource source = do
     let lexed = runLexer initState $ lexIt source
     case lexed of
         (Right tokens, st)-> do
-            let parsed = runParser (initParserState tokens) expression
+            let parsed = runParser (initParserState tokens) parseIt
             case parsed of
-                (Right expr, st) -> do
-                    eval <- runEvaluator globalEnv $ evaluate expr
+                (Right prog, st) -> do
+                    eval <- runEvaluator globalEnv $ evaluateIt prog
                     case eval of
                       Right res -> print res
                       Left err -> print err
